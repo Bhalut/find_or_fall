bdbc75ad574859fbce37278dee186336a14a7d32
KaroDev <karodev3@gmail.com>
2020-10-13 22:52:35 -0500

    Add Scene 'ConnectionTests' with basic Socket-io matchmaking (Connection.cs)

---
+++ Assets/Scripts/Connection.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using SocketIO;
+
+public class Connection : MonoBehaviour
+{
+	public SocketIOComponent socket;
+	public SocketFeedback SocketFeedbackInstance;
+
+	public void Start()
+	{
+		socket.On("open", OnOpen);
+		socket.On("error", OnError);
+		socket.On("close", OnClose);
+		socket.On("start game", OnStartGame);
+	}
+
+	public void OnOpen(SocketIOEvent e)
+	{
+		Debug.Log("[SocketIO] Open received: " + e.name + " " + e.data);
+		SocketFeedbackInstance.SetSocketID(socket.sid.ToString());
+	}
+
+	public void OnError(SocketIOEvent e)
+	{
+		Debug.Log("[SocketIO] Error received: " + e.name + " " + e.data);
+	}
+
+	public void OnClose(SocketIOEvent e)
+	{
+		Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
+		SocketFeedbackInstance.CleanData();
+	}
+
+	public void OnStartGame(SocketIOEvent e)
+	{
+		Debug.Log("[SocketIO] Match complete: " + e.name + " " + e.data);
+
+		if(e.data == null) { return; }
+
+		SocketFeedbackInstance.SetPlayersID(e.data.GetField("player1_id").str, e.data.GetField("player2_id").str);
+	}
+
+	// =======================================================================
+	public void EmitClose(SocketIOEvent e)
+	{
+		Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
+	}
+}
---
+++ Assets/Scripts/SocketFeedback.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class SocketFeedback : MonoBehaviour
+{
+	public TextMeshProUGUI socketIDTxt;
+	public TextMeshProUGUI player1IDTxt;
+	public TextMeshProUGUI player2IDTxt;
+	public string socketID;
+	public string player1ID;
+	public string player2ID;
+
+	public void SetSocketID(string id)
+	{
+		socketID = id;
+	}
+	public void SetPlayersID(string id1, string id2)
+	{
+		player1ID = id1;
+		player2ID = id2;
+	}
+
+	public void CleanData()
+	{
+		socketID = null;
+		player1ID = null;
+		player2ID = null;
+	}
+
+	public void LateUpdate()
+	{
+		socketIDTxt.text = $"socket ID: {socketID}";
+		player1IDTxt.text = $"player 1 id: {player1ID}";
+		player2IDTxt.text = $"player 2 id: {player2ID}";
+	}
+}
