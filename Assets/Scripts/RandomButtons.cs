85e40e7f02fa2de6fb6f3cfce7cce920f2757cfa
Eddy Zapata <30961017+Eddyszh@users.noreply.github.com>
2020-10-16 21:36:41 -0500

    Add main mechanic to main scene

---
+++ Assets/Scripts/RandomButtons.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomButtons : MonoBehaviour
+{
+    int button1 = 0;
+    int button2 = 0;
+    [SerializeField] GameObject[] buttons = new GameObject[8];
+    [SerializeField] GameObject gate1;
+    [SerializeField] GameObject gate2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomValue();
+    }
+
+    // Verifies if the random number is not taken yet
+    void RandomValue()
+    {
+        button1 = Random.Range(0, 8);
+        button2 = Random.Range(0, 8);
+
+        while (button1 == button2)
+        {
+            button2 = Random.Range(0, 8);
+        }
+        AssignButtonAction();
+    }
+
+    void AssignButtonAction()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (i == button1)
+            {
+                buttons[i].GetComponent<Button>().onClick.AddListener(OpenGate1);
+            }
+
+            if (i == button2)
+            {
+                buttons[i].GetComponent<Button>().onClick.AddListener(OpenGate2);
+            }
+        }
+    }
+
+    void OpenGate1()
+    {
+        gate1.GetComponent<Collider2D>().enabled = false;
+    }
+
+    void OpenGate2()
+    {
+        gate2.GetComponent<Collider2D>().enabled = false;
+    }
+}
