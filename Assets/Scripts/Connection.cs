2c64ec4ebbbb8cacb3063b24bac222a4e4f094ed
Abdel Mejia <bhalut.bear@gmail.com>
2020-10-19 21:42:23 -0500

    remove socket feedback in Connection.cs

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -1,16 +1,15 @@
 ﻿using SocketIO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class Connection : MonoBehaviour
 {
-    public static int button1;
+    public static int Button1;
     
-    public static int button2;
+    public static int Button2;
     
     public SocketIOComponent socket;
-    
-    public SocketFeedback socketFeedbackInstance;
 
     private void Awake()
     {
@@ -28,62 +27,80 @@ public class Connection : MonoBehaviour
         socket.On("opponent username", OnOpponentUsername);
     }
 
+    private void OnDisable()
+    {
+        socket.Off("open", OnOpen);
+        socket.Off("error", OnError);
+        socket.Off("close", OnClose);
+        socket.Off("opponent disconnected", OnOpponentDisconnected);
+        socket.Off("start game", OnStartGame);
+        socket.Off("my turn", OnMyTurn);
+        socket.Off("opponent username", OnOpponentUsername);
+    }
+
     public void OnOpen(SocketIOEvent e)
     {
+#if UNITY_EDITOR
         Debug.Log("[SocketIO] Open received: " + e.name + " " + e.data);
-        socketFeedbackInstance.SetSocketID(socket.sid);
+#endif
     }
 
     public void OnError(SocketIOEvent e)
     {
+#if UNITY_EDITOR
         Debug.Log("[SocketIO] Error received: " + e.name + " " + e.data);
+#endif
     }
 
     public void OnClose(SocketIOEvent e)
     {
+#if UNITY_EDITOR
         Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
-        socketFeedbackInstance.CleanData();
+#endif
     }
 
     public void OnOpponentDisconnected(SocketIOEvent e)
     {
+#if UNITY_EDITOR
         Debug.Log("Opponent Disconnected");
-
+#endif
+        
         socket.Close();
+        
         //Notification on screen and stop the game
     }
 
-    public void OnStartGame(SocketIOEvent e)
+    private void OnStartGame(SocketIOEvent e)
     {
+#if UNITY_EDITOR
         Debug.Log("[SocketIO] Match complete: " + e.name + " " + e.data);
+#endif
 
-        socketFeedbackInstance.SetPlayersID(e.data.GetField("player1_id").str, e.data.GetField("player2_id").str);
+        Button1 = int.Parse(e.data.GetField("button_1").str);
 
-        if (socket.sid == e.data.GetField("player1_id").str)
-            socketFeedbackInstance.buttonSendTurn.SetActive(true);
+        Button2 = int.Parse(e.data.GetField("button_2").str);
 
-        button1 = int.Parse(e.data.GetField("button_1").str);
-
-        button2 = int.Parse(e.data.GetField("button_2").str);
-
-        SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        SceneManager.LoadSceneAsync("Main", LoadSceneMode.Single);
         
         EmitUsername();
     }
 
     public void OnMyTurn(SocketIOEvent e)
     {
+#if UNITY_EDITOR
         Debug.Log("My Turn, the opponent send: " + e.data.GetField("button"));
+#endif
+        
         //Allow to click the buttons
     }
 
     public void OnOpponentUsername(SocketIOEvent e)
     {
+#if UNITY_EDITOR
         Debug.Log("Opponent username: " + e.data.GetField("username"));
+#endif
     }
-
-
-    // =======================================================================
+    
     public void EmitUsername()
     {
         var username = "name" + Random.Range(100, 999);
