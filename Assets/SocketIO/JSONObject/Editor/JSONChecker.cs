0b44cbcd3aa5911e8a219cbc837a4d04f3ab8996
Abdel Mejia <bhalut.bear@gmail.com>
2020-10-19 21:44:45 -0500

    clean JSONChecker.cs

--- Assets/SocketIO/JSONObject/Editor/JSONChecker.cs
+++ Assets/SocketIO/JSONObject/Editor/JSONChecker.cs
@@ -1,10 +1,12 @@
-//#define PERFTEST        //For testing performance of parse/stringify.  Turn on editor profiling to see how we're doing
+//#define PERFTEST
+////For testing performance of parse/stringify.  Turn on editor profiling to see how we're doing
 
 using UnityEngine;
 using UnityEditor;
 
-public class JSONChecker : EditorWindow {
-	string JSON = @"{
+public class JSONChecker : EditorWindow 
+{
+	private string _json = @"{
 	""TestObject"": {
 		""SomeText"": ""Blah"",
 		""SomeObject"": {
@@ -18,15 +20,20 @@ public class JSONChecker : EditorWindow {
 		""EmbeddedObject"": ""{\""field\"":\""Value with \\\""escaped quotes\\\""\""}""
 	}
 }";	  //dat string literal...
-	JSONObject j;
+
+	private JSONObject _j;
 	[MenuItem("Window/JSONChecker")]
-	static void Init() {
+	private static void Init() 
+	{
 		GetWindow(typeof(JSONChecker));
 	}
-	void OnGUI() {
-		JSON = EditorGUILayout.TextArea(JSON);
-		GUI.enabled = !string.IsNullOrEmpty(JSON);
-		if(GUILayout.Button("Check JSON")) {
+
+	private void OnGUI() 
+	{
+		_json = EditorGUILayout.TextArea(_json);
+		GUI.enabled = !string.IsNullOrEmpty(_json);
+		if(GUILayout.Button("Check JSON")) 
+		{
 #if PERFTEST
             Profiler.BeginSample("JSONParse");
 			j = JSONObject.Create(JSON);
@@ -35,17 +42,17 @@ public class JSONChecker : EditorWindow {
             j.ToString(true);
             Profiler.EndSample();
 #else
-			j = JSONObject.Create(JSON);
+			_j = JSONObject.Create(_json);
 #endif
-			Debug.Log(j.ToString(true));
+			Debug.Log(_j.ToString(true));
 		}
-		if(j) {
+		if (_j) 
+		{
 			//Debug.Log(System.GC.GetTotalMemory(false) + "");
-			if(j.type == JSONObject.Type.NULL)
-				GUILayout.Label("JSON fail:\n" + j.ToString(true));
+			if (_j.type == JSONObject.Type.NULL)
+				GUILayout.Label("JSON fail:\n" + _j.ToString(true));
 			else
-				GUILayout.Label("JSON success:\n" + j.ToString(true));
-
+				GUILayout.Label("JSON success:\n" + _j.ToString(true));
 		}
 	}
 }
