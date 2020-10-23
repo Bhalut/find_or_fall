fd95cdf45c9a3520119b9ed1c52f852b0c439bbc
Abdel Mejia <bhalut.bear@gmail.com>
2020-10-22 23:55:48 -0500

    Loader event animation

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+#pragma warning disable 618
+#pragma warning disable 649
+
+public class Loader : MonoBehaviour
+{
+    [SerializeField] private string nameScene;
+    
+    public void LoadScene()
+    {
+        SceneManager.LoadSceneAsync(nameScene, LoadSceneMode.Single);
+    }
+}
